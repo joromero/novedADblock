8a47985554500503073b4f2d0557345535c074c3
Joaquín Romero <Joromerom@gmail.com>
2018-12-06 16:19:29 -0300

    Commit inicial, bloquea cosas del openlab por Paula Guerra/keywords

---
+++ block.js
@@ -0,0 +1,32 @@
+$(window).bind("load",function(){
+	var keywords = ["emprendimiento","innovacion","innovación","openlab"];
+	var users = ["e490d6e8ba153eff83a496169d4f5d7a"];
+	var people = ["Paula Guerra G."];
+
+	if(window.location.href === "https://www.u-cursos.cl/ingenieria/2/novedades_institucion/"){
+		var divs = $("div.objeto");
+		$("div.objeto").each((ind,el) =>{
+			var div = $(el);
+			var usuario = div.find("a.usuario");
+			for(var i=0; i<users.length; i++){
+				if(usuario.attr("href").includes(users[i])){
+					div.hide();
+					console.log("Bloqueado por ser mandado por usuario ",users[i]);
+				}
+			}
+			for(var i=0; i<people.length; i++){
+				if(usuario.text().includes(people[i])){
+					div.hide();
+					console.log("Bloqueqado por ser mandado por ",people[i]);
+				}
+			}
+			for(var i=0; i<keywords.length; i++){
+				if(div.text().toLowerCase().includes(keywords[i])){
+					div.hide();
+					console.log("Bloqueado por contener keyword ",keywords[i]);
+				}
+			}
+
+		})
+	}
+});
